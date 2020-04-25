24f4b34eff16f6fb2a34aa230f6a3f8e6d8f81c5
Paul Logston <paul.logston@gmail.com>
2020-04-25 22:00:49 +0800

    Update startup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@ wget -q -O - https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo sh -c 'echo "deb http://www.apache.org/dist/cassandra/debian 311x main" > /etc/apt/sources.list.d/cassandra.list'
 sudo apt update
 sudo apt install -y mongodb python3-venv openjdk-8-jdk apt-transport-https cassandra
+if [ ! -f env ]; then
 python3 -m venv env
 ./env/bin/pip install pymongo
 ./env/bin/pip install -U pip
+fi
+echo waiting for Cassandra to start up
+sleep 5;
 nodetool status
